870354f49d3a27c817034e27fdb8b2a3b41e5188
Mac-ziis <macg286@gmail.com>
2023-10-31 18:10:38 -0700

    Add all files and directories

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApiCall.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace MvcApiCall.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly string _apikey; // New property.
+
+    // New Controller.
+    public HomeController(IConfiguration configuration)
+    {
+      _apikey = configuration["NYT"];
+    }
+
+    public IActionResult Index()
+    {
+        List<Article> allArticles = Article.GetArticles(_apikey); // Using _apikey here!
+        return View(allArticles);
+    }
+  }
+}
